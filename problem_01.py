7fbb854a6c45291136669ef663639d08069e3ae4
AldwinOda01 <>
2025-03-14 19:31:11 +0800

    Last Sync: 2025-03-14 11:31:11 (Mobile)

--- problem_01.py
+++ problem_01.py
@@ -1,10 +1,23 @@
-# YOUR NAME
+# Aldwin Odavar
 # ITELEC2
 # Problem Set 01 - Problem 01
 # Simple Calculator Program
 
 def main():
-    pass # replace this line with all of your code
+    
+    print("Simple Calculator Program")
+
+    num1 = int(input("Enter 1st number: "))
+    num2 = int(input("Enter 2nd number: "))
+
+    print(f"The sum is {num1 + num2}")
+    print(f"The difference is {num1 - num2}")
+    print(f"The product is {num1 * num2}")
+
+    if num2 != 0:
+        print(f"The quotient is {num1 / num2:.2f}")
+    else:
+        print("Division by zero is not allowed.")
 
 if __name__ == "__main__":
     main()
